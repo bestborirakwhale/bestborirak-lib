764eceee783e058ae983dec46e050b5d03448718
borira49 <borira49@aware.postbox.in.th>
2023-05-01 17:43:33 +0700

    fix: lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,27 +1,28 @@
-class Color {
-    name: string;
-    code: string;
-    constructor(name: any, code: any) {
-        this.name = name;
-        this.code = code;
-    }
-}
+// class Color {
+//     name: string;
+//     code: string;
+//     constructor(name: any, code: any) {
+//         this.name = name;
+//         this.code = code;
+//     }
+// }
 
-const allColors = [
-    new Color('brightred', '#E74C3C'),
-    new Color('soothingpurple', '#9B59B6'),
-    new Color('skyblue', '#5DADE2'),
-    new Color('leafygreen', '#48C9B0'),
-    new Color('sunkissedyellow', '#F4D03F'),
-    new Color('groovygray', '#D7DBDD'),
-];
+// const allColors = [
+//     new Color('brightred', '#E74C3C'),
+//     new Color('soothingpurple', '#9B59B6'),
+//     new Color('skyblue', '#5DADE2'),
+//     new Color('leafygreen', '#48C9B0'),
+//     new Color('sunkissedyellow', '#F4D03F'),
+//     new Color('groovygray', '#D7DBDD'),
+// ];
 
-export function getRandomColor () {
-    return allColors[Math.floor(Math.random() * allColors.length)];
-}
-export function getBlue () {
-    return allColors[2];
-}
-export function getAllColors() {
-    return allColors;
-}
+// export function getRandomColor () {
+//     return allColors[Math.floor(Math.random() * allColors.length)];
+// }
+// export function getBlue () {
+//     return allColors[2];
+// }
+// export function getAllColors() {
+//     return allColors;
+// }
+export {sayHello, sayGoodbye} from './repository/hello-world'
--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,2 @@
+"use strict";
 //# sourceMappingURL=index.test.js.map
